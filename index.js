00ce2c2efab3b6d888144c53c0ff60a2682c6c98
Mercy <maritimmercy8@gmail.com>
2025-03-14 16:11:50 +0300

    added var

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = 0;
+
+for (let i = 0; i < batteryBatches.length; i++) {
+    totalBatteries += batteryBatches[i];
+}
